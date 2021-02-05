3da65b7c40b63abd7614f65be9fccfa07069b93d
octavioduarte <octavioduarte2105@gmail.com>
2021-02-05 18:42:40 -0300

    feat: good pratices with for method

---
+++ for/02for-good-pratices.js
@@ -0,0 +1,13 @@
+// Good pratices
+
+
+//  Assign the current index value to a variable :
+
+const listNumbers = [10, 15, 20, 30];
+
+for (let i = 0; i < listNumbers.length; i++) {
+    const item = listNumbers[i];
+    const doubleNumber = item * 2;
+    console.log(`Double ${item} = ${doubleNumber}`)
+}
+
