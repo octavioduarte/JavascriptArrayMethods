9e75cf2785aac9c906b3e37fd8cff4f812271383
octavioduarte <octavioduarte2105@gmail.com>
2021-02-05 18:42:13 -0300

    feat: introdution for method

---
+++ for/01for.js
@@ -0,0 +1,10 @@
+// We use to scan an array and take a specific action for each iteration
+
+const listNumbers = [10, 15, 20, 30];
+
+for (let i = 0; i < listNumbers.length; i++) {
+    const doubleNumber = listNumbers[i] * 2;
+    console.log(`Double ${listNumbers[i]} = ${doubleNumber}`)
+}
+
+
